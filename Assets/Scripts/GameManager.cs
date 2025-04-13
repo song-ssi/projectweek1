9c7d535c0ab39d3dc632e9d71cc5ab02f5f7acc8
song-ssi <song33@outlook.kr>
2025-04-14 08:56:11 +0900

    1차 조정

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -17,7 +17,7 @@ public class GameManager : MonoBehaviour
     [SerializeField] private TextMeshProUGUI failCountText;
     [SerializeField] private TextMeshProUGUI itemCountText;
     public Image itemModeBtnImage;
-    public Slider brightnessSlider; // 버튼의 밝기 조절용
+    public Slider brightnessSlider; // 아이템 버튼의 밝기 조절용 (활성화/비활성화 표시)
     public Button itemModeBtn;
     public Button retryBtn;
     public Button homeBtn;
@@ -34,8 +34,10 @@ public class GameManager : MonoBehaviour
     public int itemcount = 10;
     float time; // 제한시간
     int failCount; // 실패 가능 횟수
-    
-
+    private float remainingRatio;
+    AudioSource audioSource;  // 오디오 소스 추가
+    public AudioClip matchSound; // 추가
+    public AudioClip failSound;
 
 
     private void Awake()
@@ -49,9 +51,10 @@ public class GameManager : MonoBehaviour
     void Start()
     {   
         Time.timeScale = 1.0f;
-        
+        // audioSource = GetComponent<AudioSource>(); // 오디오 소스 가져오기 코드 추가
         GameManager.Instance.OffItemBtn();
         LoadLevel();
+        ApplyLevelSettings(); // 불러온 레벨에 맞춰서 난이도 파라미터 세팅하기
         LoadItemCount();
         SaveItemCount();
         levelText.text = $"Lv.{level}";
@@ -59,43 +62,43 @@ public class GameManager : MonoBehaviour
 
 
         
-        if(PlayerPrefs.HasKey("passedtime"))
-        {
-            LoadTime();
-        }
-        else
-        {
-            if(level == 1)
-            {
-                passedTime = 60.0f;
-                time = 60.0f;
-                failCount = 30;
-            }
-            else if(level == 2)
-            {
-                passedTime = 60.0f;
-                time = 60.0f;
-                failCount = 20;
-            }
-            else if(level == 3)
-            {
-                passedTime = 40.0f;
-                time = 40.0f;
-                failCount = 20;
-            }
-            else if(level == 4)
-            {
-                passedTime = 40.0f;
-                time = 40.0f;
-                failCount = 10;
-            }
-            else if(level == 5)
-            {
-                passedTime = 30.0f;
-                time = 30.0f;
-                failCount = 5;
-            }
-        }
+        // if(PlayerPrefs.HasKey("passedtime"))
+        // {
+        //     LoadTime();
+        // }
+        // else
+        // {
+        //     if(level == 1)
+        //     {
+        //         passedTime = 60.0f;
+        //         time = 60.0f;
+        //         failCount = 30;
+        //     }
+        //     else if(level == 2)
+        //     {
+        //         passedTime = 60.0f;
+        //         time = 60.0f;
+        //         failCount = 20;
+        //     }
+        //     else if(level == 3)
+        //     {
+        //         passedTime = 40.0f;
+        //         time = 40.0f;
+        //         failCount = 20;
+        //     }
+        //     else if(level == 4)
+        //     {
+        //         passedTime = 40.0f;
+        //         time = 40.0f;
+        //         failCount = 10;
+        //     }
+        //     else if(level == 5)
+        //     {
+        //         passedTime = 30.0f;
+        //         time = 30.0f;
+        //         failCount = 5;
+        //     }
+        // }
         
 
         
@@ -110,13 +113,18 @@ public class GameManager : MonoBehaviour
         timeBar.localScale = new Vector3(-(float.Parse(timeText.text) / time), 1.0f, 1.0f);
         failCountText.text = $"실패 가능 횟수 : {failCount}";
         itemCountText.text = $"X {itemcount}";
-        
+        remainingRatio = (passedTime / time);
+
         if(passedTime < 0.0f)
         {
             GameOver();
         }
-
-        if(passedTime < 6.0f)
+        else if(remainingRatio < 0.5f) 
+        {
+            Image img = timeBar.GetComponent<Image>();
+            img.color = Color.yellow;
+        }
+        else if(remainingRatio < 0.3f) // 비율 기준 계산이 아니라 초 기준 계산이어서 코드 수정함 
         {
             Image img = timeBar.GetComponent<Image>();
             img.color = Color.red;
@@ -124,6 +132,47 @@ public class GameManager : MonoBehaviour
 
         }
     }
+
+    private void ApplyLevelSettings() // 새로 추가한 코드 여기서 난이도 관리
+    {
+        switch (level)
+        {
+            case 1:
+                 passedTime = 60.0f;
+                 time = 60.0f;
+                 failCount = 30;
+                 break;
+
+            case 2:
+                passedTime = 60.0f;
+                time = 60.0f;
+                failCount = 20;
+                break;
+
+            case 3:
+                passedTime = 40.0f;
+                time = 40.0f;
+                failCount = 20;
+                break;
+
+            case 4:
+                passedTime = 40.0f;
+                time = 40.0f;
+                failCount = 10;
+                break;
+
+            case 5:
+                passedTime = 30.0f;
+                time = 30.0f;
+                failCount = 5;
+                break;
+
+            default:
+                Debug.LogWarning("정의되지 않은 레벨입니다.");
+                break;
+        }
+    }
+    
 
     // 아이템 버튼 온/오프
     public void OnItemBtn()
@@ -168,6 +217,7 @@ public class GameManager : MonoBehaviour
     {
         if(firstCard.idx == secondCard.idx)
         {
+            audioSource.PlayOneShot(matchSound); // 카드 매칭 사운드 한 번만 출력
             firstCard.DestroyCard();
             secondCard.DestroyCard();
             cardCount -= 2;
@@ -181,6 +231,7 @@ public class GameManager : MonoBehaviour
         }
         else
         {
+            // audioSource.PlayOneShot(failSound);
             firstCard.CloseCard();
             secondCard.CloseCard();
             failCount --;
